3dd0696eaec93aacf342e68d0ec3b9d3d896b26d
Рома Хмарук <rkhmaruk4@gmail.com>
2019-11-29 14:58:11 +0300

    Set up Webpack

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-export default (a, b) => a + b;
+import React from 'react';
+import ReactDom from 'react-dom';
+import './index.css';
+import App from './components/App';
+
+ReactDom.render(<App />, document.getElementById('root'));
---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  mode: "development",
+  output: {
+    filename: "./main.js"
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "/dist"),
+    compress: true,
+    port: 9000,
+    watchContentBase: true,
+    progress: true
+  },
+
+  module: {
+    rules: [
+	{
+		test: /\.jsx?$/,
+		exclude: /node_modules/,
+		use: [{
+			loader: 'babel-loader',
+			options: {
+				presets:['@babel/preset-env','@babel/preset-react']
+			}
+		}],
+	},
+	{
+		test: /\.js$/,
+		exclude: /node_modules/,
+		loader: "babel-loader"
+	},
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ["file-loader"]
+      }
+    ]
+  },
+  resolve: {
+	extensions: [ '.js', '.jsx', '.css', '.web.js', '.mjs', '.json', '.web.jsx'],
+	modules: [
+		"node_modules"
+	]
+  }
+//   plugins: [
+//     new HtmlWebpackPlugin({
+//       template: "./src/index.html"
+//     })
+//   ]
+};
