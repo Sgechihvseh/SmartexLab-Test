271f52cc9cc41ff9b2ee7498cd38a2086cc2a341
Рома Хмарук <rkhmaruk4@gmail.com>
2019-12-06 20:55:28 +0300

    Add CSS-loader in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -27,14 +27,21 @@ module.exports = {
 		}],
 	},
 	{
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+    },
+	{
 		test: /\.js$/,
 		exclude: /node_modules/,
 		loader: "babel-loader"
 	},
-      {
+    {
         test: /\.(png|svg|jpg|gif)$/,
         use: ["file-loader"]
-      }
+	}
     ]
   },
   resolve: {
